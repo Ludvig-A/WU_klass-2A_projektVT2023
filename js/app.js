e9c8b352537fd33b7a663750ea9526a17446be13
Ludvig-Almgren-nti-johanneberg <ludvig.almgrennylen@elev.ga.ntig.se>
2023-04-02 20:12:16 +0200

    uppdateringar

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log("hej hej")
+
+let button1 = document.querySelector(".h2tqp").addEventListener("click", showmenu1);
+function showmenu1() {
+    let productimagetext1 = document.querySelector(".product-imagetext1");
+    productimagetext1.classList.toggle("hidden");
+    let productimage
+}
+
+let button2 = document.querySelector(".h2fwc").addEventListener("click", showmenu2);
+function showmenu2() {
+    let productimagetext2 = document.querySelector(".product-imagetext2");
+    productimagetext2.classList.toggle("hidden");
+    let productimage
+}
+
+let button3 = document.querySelector(".h2mei").addEventListener("click", showmenu3);
+function showmenu3() {
+    let productimagetext3 = document.querySelector(".product-imagetext3");
+    productimagetext3.classList.toggle("hidden");
+    let productimage
+}
+
